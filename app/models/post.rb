2dc837d8452cb56cfebb4ee3c2c2f07539ecff9e
Corey Norris & Paul Ogawa <student@epicodus.com>
2014-04-10 16:28:21 -0700

    broken image will no longer appear on pages without photos

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   validates :name, presence: true
   validates :description, presence: true
 
-  has_attached_file :photo, :styles => {:thumb => "150x150>" },
+  has_attached_file :photo, :styles => {:thumb => "150x150>", :medium => "300x300>" },
     :url => "/assets/products/:id/:style/:basename.:extension",
     :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png"]
