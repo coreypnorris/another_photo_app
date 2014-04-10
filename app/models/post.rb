371cb9cd152bc13f33a3366ce298d1b59846d0dd
Corey Norris & Paul Ogawa <student@epicodus.com>
2014-04-10 16:06:58 -0700

    images will now be stored in the public/assets folder

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ActiveRecord::Base
   validates :name, presence: true
   validates :description, presence: true
 
-  has_attached_file :photo, :styles => {:thumb => "150x150>" }
+  has_attached_file :photo, :styles => {:thumb => "150x150>" },
+    :url => "/assets/products/:id/:style/:basename.:extension",
+    :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
