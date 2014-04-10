f399d9a3d00f7095c5478a4c2f875bbd77a60a7e
Corey Norris & Paul Ogawa <student@epicodus.com>
2014-04-10 16:00:08 -0700

    adds imagemagick resizing and thumbnail size option

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,6 @@ class Post < ActiveRecord::Base
   validates :name, presence: true
   validates :description, presence: true
 
-  has_attached_file :photo
+  has_attached_file :photo, :styles => {:thumb => "150x150>" }
   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png"]
-
 end
